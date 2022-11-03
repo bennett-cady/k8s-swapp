d4a6fb593139a42a650e29c24f2f551c45e6bded
ben_cady <179367@ust-global.com>
2022-11-03 14:11:52 -0600

    implemented and tested heapify(), appears to be work

--- src/main/java/AbstractDataTypes/MaxHeap.java
+++ src/main/java/AbstractDataTypes/MaxHeap.java
@@ -23,6 +23,13 @@ public class MaxHeap
 		mheap = new int[max];
 	}
 	
+	public MaxHeap(int[] arr) {
+		mheap=arr;
+		this.size=arr.length;
+		this.max=arr.length;
+		this.heapify();
+	}
+	
 	public void push(int n)
 	{
 		mheap[this.size]=n;
@@ -122,70 +129,54 @@ public class MaxHeap
 		return all;
 	}
 	
-	/*
-	public void heapify(int[] arr) {
-		this.size = arr.length;
-		int parent = arr.length/2 -1;
-		while(parent>=0) {
+	
+	public void heapify() 
+	{
+		int parent = this.size/2 -1;
+		while(parent>=0){
 			this.percDown(parent);
 			parent--;
 		}
 	}
-	// requires working percDown method
+	
+	
+	/*
+	public static int[] heapSort(int[] arr) {
+	}
 	*/
 	
 	
 	public static void main(String[] args) {
-		MaxHeap test = new MaxHeap(90);
-		test.push(100);
-		test.push(16);
-		test.push(76);
-		test.push(7);
-		System.out.println(test.size);
-		System.out.println(test.printHeap());
-		test.push(18);
-		System.out.println(test.printHeap());
+		//MaxHeap test = new MaxHeap(90);
+		int[] heap = {16, 23, 3, 45, 7, 100, 65, 19, 22};		
+		MaxHeap mh = new MaxHeap(heap);
+		System.out.println(mh.printHeap());
+		// 100, 45, 65, 23, 7, 3, 16, 19, 22
 		
-		test.push(24);
-		System.out.println(test.printHeap());
-		test.push(301);
-		System.out.println(test.printHeap());
-		test.push(55);
-		System.out.println(test.printHeap());
+		int[] heap1 = {1, 3, 2, 4, 6, 5, 8, 7};		
+		MaxHeap mh1 = new MaxHeap(heap1);
+		System.out.println(mh1.printHeap());
+		//8, 7, 5, 4, 6, 1, 2, 3
 		
-		test.push(981);
-		 
+		int[] heap2 = {1, 3};		
+		MaxHeap mh2 = new MaxHeap(heap2);
+		System.out.println(mh2.printHeap());
+		//3, 1
 		
-		System.out.println(test.printHeap());
-		test.deleteRoot();
-		System.out.println(test.printHeap());
+		int[] heap3 = {1};		
+		MaxHeap mh3 = new MaxHeap(heap3);
+		System.out.println(mh3.printHeap());
+		//1
 		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
+		int[] heap4 = {};		
+		MaxHeap mh4 = new MaxHeap(heap4);
+		System.out.println(mh4.printHeap());
+		//Nothing
+		System.out.println("*************");
 		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
-		test.deleteRoot();
-		System.out.println(test.printHeap());
-		
+		int[] bigHeap = {164, 223, 15, 905, 67, 333, 721, 202, 4, 5, 1009, 245, 408, 125, 90, 880, 127, 134, 398};
+		MaxHeap large = new MaxHeap(bigHeap);
+		System.out.println(large.printHeap());
 	}
 
 }
