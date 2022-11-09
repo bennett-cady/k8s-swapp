40a1906620b818c8c6d360e375a2195b21cb7c5d
bennett-cady <112588952+bennett-cady@users.noreply.github.com>
2022-11-08 23:21:51 -0700

    Update buildAndDeploy.sh
    Changed image name, included updated namespace in kubectl apply command

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -2,12 +2,12 @@
 
 # mvn clean package
 
-docker build -t benjcady14/number-ops002 .
+docker build -t benjcady14/mixed_dem0:0.0.1 .
 
-docker push benjcady14/number-ops002
+docker push benjcady14/mixed_demo:0.0.1
 
 ## if you have the application deployed in a kubernetes cluster
-# kubectl apply -f kubernetes/Deployment.yaml
+# kubectl apply -f kubernetes/Deployment.yaml -n web-app-demo
 
 ## image pull policy is set to always
 ## it will pull the updated version of the image
