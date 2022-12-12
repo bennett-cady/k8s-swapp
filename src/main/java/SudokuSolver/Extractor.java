68f59f33b7207a3c17c13fbf813e36c821d78860
ben_cady <179367@ust-global.com>
2022-12-11 20:01:48 -0700

    added method to return the box given by a row, coumn number

--- src/main/java/SudokuSolver/Extractor.java
+++ src/main/java/SudokuSolver/Extractor.java
@@ -54,11 +54,38 @@ public class Extractor {
 				return 0;
 			}else if(col>=3 && col<=5) {
 				return 1;
-			} else {
-				return 2;
-			}
+			} else {return 2;}
+		} else if(row>=3 && row<=5) 
+		{
+			if(col<=2 && col>=0) {
+				return 3;
+			}else if(col>=3 && col<=5) {
+				return 4;
+			} else {return 5;}			
+		} 
+		if(col<=2 && col>=0) {
+			return 6;
+		}else if(col>=3 && col<=5) {
+			return 7;
+		} 
+		return 8;
+	}
+	
+	public static void main(String[] args) {
+		int b1 = boxByRowColumn(4, 0);
+		int b2 = boxByRowColumn(1, 5);
+		int b3 = boxByRowColumn(8, 8);
+		int b4 = boxByRowColumn(6,4);
+		int[] boxes = {b1, b2, b3, b4};
+		for(int b: boxes) {
+			System.out.println(b);
 		}
-		return 0;
+		/*
+		 * 3
+		 * 1
+		 * 8
+		 * 7
+		 */
 	}
 
 }
