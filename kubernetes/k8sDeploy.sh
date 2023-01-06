0c5653e59ab143ec59b882200707a2bb9851e8b3
ben_cady <179367@ust-global.com>
2023-01-06 12:09:41 -0700

    changed namespace in deployment script

--- kubernetes/k8sDeploy.sh
+++ kubernetes/k8sDeploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+## If Ingress not present 
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 
 ## If namespace does not exist already
 # kubectl create namespace web-app-demo
 
-kubectl apply -f Ingress.yaml -n web-app-demo
+kubectl apply -f Ingress.yaml -n mixed-demo
 
-kubectl apply -f Service.yaml -n web-app-demo
+kubectl apply -f Service.yaml -n mixed-demo
 
-kubectl apply -f Deployment.yaml -n web-app-demo
+kubectl apply -f Deployment.yaml -n mixed-demo
 
-kubectl get po -n web-app-demo -w 
+kubectl get po -n mixed-demo -w 
