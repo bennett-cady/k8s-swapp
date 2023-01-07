b75f365352267232125f0ad0c2de2d4dc86e4e0e
ben_cady <179367@ust-global.com>
2023-01-07 15:22:44 -0700

    added useful minikube command to deployment script

--- kubernetes/k8sDeploy.sh
+++ kubernetes/k8sDeploy.sh
@@ -4,12 +4,14 @@
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 
 ## If namespace does not exist already
-# kubectl create namespace web-app-demo
+# kubectl create namespace mixed-demo
 
 ## Ingress is not used when application is deployed on minkube
+## kubectl delete ing swapp-ing -n mixed-demo
 # kubectl apply -f Ingress.yaml -n mixed-demo
 
 kubectl apply -f Service.yaml -n mixed-demo
+# minikube service simple-web app-service --url -n mixed-demo
 
 kubectl apply -f Deployment.yaml -n mixed-demo
 
